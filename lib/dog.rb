4631f5b6820107fe7e0c3dfdb06e6178412ffbb6
Eric Mayfield <eric@visiblycreative.com>
2018-11-01 18:12:35 +0000

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,25 @@
+require 'pry'
+
+class Dog
+  attr_reader :name
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all.each { |dog|
+      puts dog.name
+    }
+    @@all
+  end
+
+  def self.clear_all
+    @@all = []
+  end
+end
+
+lab = Dog.new("Jenni")
+Dog.all
